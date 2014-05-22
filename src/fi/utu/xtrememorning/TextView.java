eca6beea00f1f5afd753b9597c0629cf2f26fa42
Miika Vihersaari <miika.vihersaari@gmail.com>
2014-05-22 14:21:04 +0300

    This is actually an amend. Added TextView and View.

---
+++ src/fi/utu/xtrememorning/TextView.java
@@ -0,0 +1,12 @@
+package fi.utu.xtrememorning;
+
+
+/**
+ * A text-based view for rendering the GameState.
+ */
+public class TextView implements View
+{
+	public void RenderState() {
+		System.out.println("Rendering GameState, hang on!");
+	}
+}
---
+++ src/fi/utu/xtrememorning/View.java
@@ -0,0 +1,10 @@
+package fi.utu.xtrememorning;
+
+
+/**
+ * The interface for all views.
+ */
+public interface View
+{
+	void RenderState();
+}
