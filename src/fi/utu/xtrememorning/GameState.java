f29fe5ad5059218399f9a6a82dc5a87a5cb234a0
Santeri Toikka <santeri.toikka@iki.fi>
2014-05-22 14:53:42 +0300

    Changes to GameState.

--- src/fi/utu/xtrememorning/GameState.java
+++ src/fi/utu/xtrememorning/GameState.java
@@ -4,8 +4,33 @@ import java.util.ArrayList;
 
 public class GameState {
 	ArrayList<Door> doors = new ArrayList<Door>();
+	ArrayList<Room> rooms = new ArrayList<Room>();
+	ArrayList<Player> players = new ArrayList<Player>();
 	
 	public GameState(GameData data) {
 		
 	}
+	
+	/**
+	 * Return all doors in the given room.
+	 * @param room
+	 * @return
+	 */
+	public ArrayList<Door> getDoors(Room room) {
+		return doors;
+	}
+	
+	/**
+	 * @return All doors in the game.
+	 */
+	public ArrayList<Room> getRooms() {
+		return rooms;
+	}
+	
+	public ArrayList<Player> getPlayers() {
+		return players;
+	}
+	
+	
+	
 }
