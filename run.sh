6059a0482d19e602d0ba3d14978176d7003f2661
Santeri Toikka <santeri.toikka@iki.fi>
2014-06-09 13:13:29 +0300

    Added MySQL J connector to project.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #/bin/java
 # run expects to find a binary directory, which is autocompiled by eclipse
-CLASSPATH="${PWD}/bin"
+CLASSPATH="${PWD}/bin:${PWD}/lib/mysql-connector-java-5.1.31-bin.jar"
 java -classpath ${CLASSPATH} fi.utu.xtrememorning.MainGame
